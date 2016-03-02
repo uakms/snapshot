deef2f308092834956b1490268b090863841b9a9
nakinor <nakinor@gmail.com>
2016-03-02 21:40:08 +0900

    Cr

--- tools/jisyo-converter.rb
+++ tools/jisyo-converter.rb
@@ -1,148 +1,190 @@
-#!/usr/local/bin/ruby -EUTF-8
 # Author: nakinor
 # Created: 2014-03-20
-# Revised: 2016-01-29
+# Revised: 2016-03-02
 
-require 'open-uri'
-
-#MTODICT_SITE = "https://raw.githubusercontent.com/nakinor/mto/master"
-MTODICT_SITE = ENV['MTODIC']
-KANA_JISYO   = MTODICT_SITE + "/kana-jisyo"
-KANJI_JISYO  = MTODICT_SITE + "/kanji-jisyo"
-HANGEUL_JISYO  = MTODICT_SITE + "/hangeul-jisyo"
+MTODICT_DIR   = ENV['MTODIC']
+KANA_JISYO    = MTODICT_DIR + "/kana-jisyo"
+KANJI_JISYO   = MTODICT_DIR + "/kanji-jisyo"
+HANGEUL_JISYO = MTODICT_DIR + "/hangeul-jisyo"
 
 def split_comment(string)
   return string.sub("[", "[\"").gsub(/\|/, "\", \"").sub("]", "\"]")
 end
 
-def create_array_json(use_jisyo)
-  tmp_arr = []
-  open(use_jisyo, "r") { |io|
-    while line = io.gets
-      unless line =~ /^;.*|^$/
-        string = line.sub(/(\s+;.*)/, "").chomp("\n")
-        if $1 != nil
-          commentString = $1.sub(/(\[.*\])/, "")
-          #コメントの [hoge|fuga] 部分を取り出して次で使うためのダミー
+
+############################################################################
+# 新型の JSON を出力するクラス
+class MtoNewDict
+  #二次元配列を作成する
+  def initialize(use_jisyo)
+    @dicarr = []
+    open(use_jisyo, "r") { |io|
+      while line = io.gets
+        unless line =~ /^;.*|^$/
+          string = line.sub(/(\s+;.*)/, "").chomp("\n")
+          if $1 != nil
+            commentString = $1.sub(/(\[.*\])/, "")
+            #コメントの [hoge|fuga] 部分を取り出して次で使うためのダミー
+          end
+          comment = $1.to_s
+          #comment が nil であっても .to_s をすると "" が得られてエラー回避
+          if comment.size == 0
+            comment = "[]"
+          else
+            comment = split_comment(comment)
+          end
+          string2 = string + " /" + comment
+          elems = "\"" + string2.gsub(/\s* \//, "\", \"").sub(/\"\[/, "[")
+          @dicarr << elems
         end
-        comment = $1.to_s
-        #comment が nil であっても .to_s をすると "" が得られてエラー回避
-        if comment.size == 0
-          comment = "[]"
-        else
-          comment = split_comment(comment)
+      end
+    }
+  end
+
+  # 新型の JSON を出力する
+  # 例  ["そう", "さう", []],
+  #     ["り", "리", ["理", "里", "李", "裏", "裡"]],
+  def output_json(pref="pref")
+    tmp_arr = []
+    @dicarr.each { |x|
+      tmp_arr << "[" + x + "]"
+    }
+    tmp_str = tmp_arr.join('_')
+    return "var #{pref} \=\n[\n  " + tmp_str.gsub(/\]_/, "\],\n  ") + "\n]"
+  end
+end
+
+
+############################################################################
+# 旧型の各種フォーマットを出力するクラス
+class MtoDict
+  # 二次元配列を作成する
+  def initialize(use_jisyo)
+    @dicarr = []
+    open(use_jisyo, "r") { |io|
+      while line = io.gets
+        unless line =~ /^;.*|^$/
+          string = line.sub(/\s+;.*/, "").chomp("\n")
+          elems = string.split(/\s* \//)
+          @dicarr << elems
         end
-        string2 = string + " /" + comment
-        elems = "\"" + string2.gsub(/\s* \//, "\", \"").sub(/\"\[/, "[")
-        tmp_arr << elems
       end
-    end
-  }
-  return tmp_arr
-end
+    }
+  end
 
-def output_array_json(dicarr, pref)
-  tmp_arr = []
-  dicarr.each { |x|
-    tmp_arr << "[" + x + "]"
-  }
-  tmp_str = tmp_arr.join('_')
-  return "var #{pref} \=\n[\n  " + tmp_str.gsub(/\]_/, "\],\n  ") + "\n]"
-end
-
-def create_dicarr(use_jisyo)
-  tmp_arr = []
-  open(use_jisyo, "r") { |io|
-    while line = io.gets
-      unless line =~ /^;.*|^$/
-        string = line.sub(/\s+;.*/, "").chomp("\n")
-        elems = string.split(/\s* \//)
-        tmp_arr << elems
-      end
-    end
-  }
-  return tmp_arr
-end
-
-def output_json(dicarr)
-  tmp_arr = []
-  dicarr.each { |x|
-    tmp_arr << "\"#{x[0]}\"" + ": " +  "\"#{x[1]}\""
-  }
-  tmp_str = tmp_arr.join(',')
-  return "{\n  " + tmp_str.gsub(/\",/, "\",\n  ") + "\n}"
-end
-
-def output_json_t(dicarr, pref)
-  tmp_arr = []
-  dicarr.each { |x|
-    tmp_arr << "\"#{x[1]}\"" + ": " +  "\"#{x[0]}\""
-  }
-  tmp_str = tmp_arr.join(',')
-  return "var #{pref} \=\n{\n  " + tmp_str.gsub(/\",/, "\",\n  ") + "\n}"
-end
-
-def output_json_n(dicarr, pref)
-  tmp_arr = []
-  dicarr.each { |x|
-    tmp_arr << "\"#{x[0]}\"" + ": " +  "\"#{x[1]}\""
-  }
-  tmp_str = tmp_arr.join(',')
-  return "var #{pref} \=\n{\n  " + tmp_str.gsub(/\",/, "\",\n  ") + "\n}"
-end
-
-def output_tsv(dicarr)
-  tmp_arr = []
-  dicarr.each { |x|
-    tmp_arr << "#{x[0]}" + "\t" +  "#{x[1]}"
-  }
-  return tmp_arr
-end
-
-def output_csv(dicarr)
-  tmp_arr = []
-  dicarr.each { |x|
-    tmp_arr << "#{x[0]}" + "," +  "#{x[1]}"
-  }
-  return tmp_arr
-end
-
-def write_file(ofile, jisyo)
-  File::open("#{ofile}", "w") { |f|
-    if ofile =~ /(dic.*)|.json/
-      case $1
-      when "dic-kana.json"
-        f.puts output_array_json(create_array_json(jisyo), "kanaArray")
-      when "dic-kanji.json"
-        f.puts output_array_json(create_array_json(jisyo), "kanjiArray")
-      when "dic-hangeul.json"
-        f.puts output_array_json(create_array_json(jisyo), "hanArray")
-      else
-        f.puts output_json(create_dicarr(jisyo))
-      end
-    elsif
-      ofile =~ /csv/
-      f.puts output_csv(create_dicarr(jisyo))
-    elsif
-      ofile =~ /tsv/
-      f.puts output_tsv(create_dicarr(jisyo))
+  # JSON 形式で出力するメソッド
+  # 例  flag が 'r' 以外の時  "そう": "さう",
+  #     flag が 'r'     の時  "さう": "そう",
+  def output_json(flag="n")
+    tmp_arr = []
+    if flag != "r"
+      @dicarr.each { |x| tmp_arr << "\"#{x[0]}\"" + ": " + "\"#{x[1]}\""}
     else
-      puts "知らんがな（´・ω・｀）"
-      exit 1
+      @dicarr.each { |x| tmp_arr << "\"#{x[1]}\"" + ": " + "\"#{x[0]}\""}
     end
+    tmp_str = tmp_arr.join(',')
+    return "{\n  " + tmp_str.gsub(/\",/, "\",\n  ") + "\n}"
+  end
+
+  # プレフィックス付きの JSON 形式で出力するメソッド
+  # 例  flag が 'r' 以外の時  "そう": "さう",
+  #     flag が 'r'     の時  "さう": "そう",
+  def output_pref_json(pref, flag="n")
+    tmp_arr = []
+    if flag != "r"
+      @dicarr.each { |x| tmp_arr << "\"#{x[0]}\"" + ": " + "\"#{x[1]}\""}
+    else
+      @dicarr.each { |x| tmp_arr << "\"#{x[1]}\"" + ": " + "\"#{x[0]}\""}
+    end
+    tmp_str = tmp_arr.join(',')
+    return "var #{pref} \=\n{\n  " + tmp_str.gsub(/\",/, "\",\n  ") + "\n}"
+  end
+
+  # TSV 形式で出力するメソッド
+  # 例  flag が 'r' 以外の時  そう	さう
+  #     flag が 'r'     の時  さう	そう
+  def output_tsv(flag="n")
+    tmp_arr = []
+    if flag != "r"
+      @dicarr.each { |x| tmp_arr << "#{x[0]}" + "\t" + "#{x[1]}" }
+    else
+      @dicarr.each { |x| tmp_arr << "#{x[1]}" + "\t" + "#{x[0]}" }
+    end
+    return tmp_arr
+  end
+
+  # CSV 形式で出力するメソッド
+  # 例  flag が 'r' 以外の時  そう,さう
+  #     flag が 'r'     の時  さう,そう
+  def output_csv(flag="n")
+    tmp_arr = []
+    if flag != "r"
+      @dicarr.each { |x| tmp_arr << "#{x[0]}" + "," + "#{x[1]}"}
+    else
+      @dicarr.each { |x| tmp_arr << "#{x[1]}" + "," + "#{x[0]}"}
+    end
+    return tmp_arr
+  end
+
+  # YAML 形式で出力するメソッド
+  # 例  flag が 'r' 以外の時  そう: [さう]
+  #     flag が 'r'     の時  さう: [そう]
+  def output_yml(flag="n")
+    tmp_arr = []
+    if flag != "r"
+      @dicarr.each { |x| tmp_arr << "#{x[0]}" + ": [" + "#{x[1]}" + "]"}
+    else
+      @dicarr.each { |x| tmp_arr << "#{x[1]}" + ": [" + "#{x[0]}" + "]"}
+    end
+    return tmp_arr
+  end
+end
+
+
+############################################################################
+# 標準出力ではなくファイルに直接書き込む
+def write_file(ofile, data)
+  File::open("#{ofile}", "w") { |f|
+    f.puts data
   }
 end
 
+
+############################################################################
 def main()
-  write_file("dic-kana.json", KANA_JISYO)
-  write_file("dic-kanji.json", KANJI_JISYO)
-  write_file("dic-hangeul.json", HANGEUL_JISYO)
-  # write_file("kana-jisyo.json", KANA_JISYO)
-  # write_file("kanji-jisyo.json", KANJI_JISYO)
-  # write_file("kana-jisyo.csv", KANA_JISYO)
-  # write_file("kanji-jisyo.csv", KANJI_JISYO)
-  # write_file("kana-jisyo.tsv", KANA_JISYO)
-  # write_file("kanji-jisyo.tsv", KANJI_JISYO)
+  kana_data = MtoNewDict.new(KANA_JISYO).output_json("kanaArray")
+  write_file("dic-kana.json", kana_data)
+
+  kanji_data = MtoNewDict.new(KANJI_JISYO).output_json("kanjiArray")
+  write_file("dic-kanji.json", kanji_data)
+
+  hange_data = MtoNewDict.new(HANGEUL_JISYO).output_json("hanArray")
+  write_file("dic-hangeul.json", hange_data)
 end
 
 main()
+
+# puts MtoDict.new(KANA_JISYO).output_json("n")
+# puts MtoDict.new(KANA_JISYO).output_json("r")
+# puts MtoDict.new(KANJI_JISYO).output_json("n")
+# puts MtoDict.new(KANJI_JISYO).output_json("r")
+
+# puts MtoDict.new(KANA_JISYO).output_pref_json("kana","n")
+# puts MtoDict.new(KANA_JISYO).output_pref_json("kana", "r")
+# puts MtoDict.new(KANJI_JISYO).output_pref_json("kanji", "n")
+# puts MtoDict.new(KANJI_JISYO).output_pref_json("kanji", "r")
+
+# puts MtoDict.new(KANA_JISYO).output_csv("n")
+# puts MtoDict.new(KANA_JISYO).output_csv("r")
+# puts MtoDict.new(KANJI_JISYO).output_csv("n")
+# puts MtoDict.new(KANJI_JISYO).output_csv("r")
+
+# puts MtoDict.new(KANA_JISYO).output_tsv("n")
+# puts MtoDict.new(KANA_JISYO).output_tsv("r")
+# puts MtoDict.new(KANJI_JISYO).output_tsv("n")
+# puts MtoDict.new(KANJI_JISYO).output_tsv("r")
+
+# puts MtoDict.new(KANA_JISYO).output_yml("n")  # nvcheck 仮名の旧から新向け
+# puts MtoDict.new(KANA_JISYO).output_yml("r")  # nvcheck 仮名の新から旧向け
+# puts MtoDict.new(KANJI_JISYO).output_yml("n") # nvcheck 漢字の旧から新向け
+# puts MtoDict.new(KANJI_JISYO).output_yml("r") # nvcheck 漢字の新から旧向け
